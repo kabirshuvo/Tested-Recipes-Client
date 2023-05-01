9438894044d894ecfa41c6eff9045a928548de25
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-01 21:57:44 +0600

    initial setup ready with firebase installation and firebseconfig file add. and .env.local file created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjucp0JJDR9wsG3F5zA1IxOrwUH7_moYQ",
+  authDomain: "tested-recipes.firebaseapp.com",
+  projectId: "tested-recipes",
+  storageBucket: "tested-recipes.appspot.com",
+  messagingSenderId: "27139863733",
+  appId: "1:27139863733:web:f79a73d56b3ea6b230bafc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
