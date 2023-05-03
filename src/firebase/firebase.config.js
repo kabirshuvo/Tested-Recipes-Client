0eeb6821461ed3f938b3194bf7a016709575dca9
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-03 18:36:07 +0600

    .env.local initiated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 console.log('environment variable', import.meta.env.VITE_PASS)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjucp0JJDR9wsG3F5zA1IxOrwUH7_moYQ",
-  authDomain: "tested-recipes.firebaseapp.com",
-  projectId: "tested-recipes",
-  storageBucket: "tested-recipes.appspot.com",
-  messagingSenderId: "27139863733",
-  appId: "1:27139863733:web:f79a73d56b3ea6b230bafc"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
